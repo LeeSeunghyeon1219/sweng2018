97dea9b80021b328bf1700dc5a6f7826d513fbe1
Seunghyeon Lee <5644718@naver.com>
2018-10-16 01:02:47 -0700

    added sss

--- myapp.c
+++ myapp.c
@@ -1,12 +1,17 @@
 #include "func2.h"
 #include "func1.h"
 #include <stdio.h>
+void sss()
+{
+	pirntf("My name is Seunghyeon\n");
+}
 void fff()
 {
 	printf("It's too fast!\n");
 }
 int main(void)
 {
+	sss();
 	fff();
 	func1();
 	func2();
